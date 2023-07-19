0b252362f5192be742fff281cc825e1fc57b9ddc
Tae C <62630025+TaeChung@users.noreply.github.com>
2023-07-19 19:48:45 -0400

    Merging resolution

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Welcome fellow Crewmates!
 
-    Welcome to the official website of Brews and Crews, where loved ones (and their barely tolerated add-ons) can mingle together over
-    the simple joys of fine beer. Did you know that almost every single ethnic group has made their own varieties of beer throughout the ages?
+    Welcome to the official website of Brews and Crews, where loved ones (and their barely tolerated add-ons) 
+    can mingle together over the simple joys of fine beer. Did you know that almost every single ethnic group 
+    has made their own varieties of beer throughout the ages?
+
     Now you too can share in our ancestral heritage of gathering around and sharing news at the meal table.
+
+# Our Company
+
+    Brews and Crews was the culmination of desire and practicality. With our wonderful API, you can find up to 
+    50 breweries across the 48 continuous states of good -ol America, letting you appreciate
 
 # The Trio of Devs
 
@@ -10,7 +17,26 @@
 
     Cynthia Pian, our team leader and overall manager of the this project.
 
-    Emmanuel Barroso, Developer (CSS, Javascript, index.html)
+    Emmanuel Barroso, Developer(Dev) (CSS, Javascript, index.html)
 
-    Tae Chung, Developer, (index.html, CSS)
+    Tae Chung, Developer(Dev), (index.html, CSS, Javascript)
 
+# Development: 1
+
+    First Cynthia Pian laid the groundwork by setting up the repository for our website, https://github.com/CynthiaP7/Brewery-Project1-Group6. 
+    Then, Developer Tae set up the basic code for the index.html, while Develoepr Emmanuel did the same for CSS and Javascript.
+    And then the Trio found the geolocation and brewery APIs.
+
+# Development: 2
+
+    One of most arduous hurdles we encountered was the position of the map. At first, it was off center.
+    Eventually, Developer Emmanuel fixed this after altering the index.html and script.js files.
+
+# Development: 3
+
+    Another significant hurdle we encountered was the navigation links being a dark grey
+    (thus hard to see), the navbar was manipulated to turn the text white.
+    The other texts were turned white by adding color:white; to the style.css 'body' file.
+
+    
+g
--- assets/script.js
+++ assets/script.js
@@ -1,135 +1,127 @@
-  const x = document.getElementById("search");
+const x = document.getElementById("search");
 
-  function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-      "<br>Longitude: " + position.coords.longitude;
-  }
-  
-  const brewLocation = 'https://api.openbrewerydb.org/v1/breweries';
-  
-  fetch(brewLocation)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      initMap(data);
-    })
-    .catch(error => {
-      console.error(error);
-      alert("Failed to fetch brewery data.");
-    });
-  
-  // Initialize and add the map
-  let map;
-  
-  async function initMap(breweries) {
-    const position = { lat: 40.776, lng: -73.971 };
-    const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-  
-    map = new Map(document.getElementById("map"), {
-      zoom: 13,
-      center: position,
-      mapId: "DEMO_MAP_ID",
-    });
-  
-    const searchButton = document.getElementById("search-button");
-    searchButton.addEventListener("click", handleSearch);
-  
-    function handleSearch(event) {
-      event.preventDefault();
-      const searchInput = document.getElementById("search-input");
-      const searchQuery = searchInput.value.trim().toLowerCase();
-  
-      if (searchQuery === "") {
-        alert("Please enter a state.");
-        return;
-      }
-  
-      const filteredBreweries = breweries.filter(brewery => {
-        const state = brewery.state.toLowerCase();
-        return state.includes(searchQuery);
-      });
-  
-      if (filteredBreweries.length === 0) {
-        alert("No breweries found in this location.");
-        return;
-      }
-  
-      showBreweries(filteredBreweries);
-      focusMap(filteredBreweries);
-    }
-  
-    function showBreweries(breweries) {
-      breweries.forEach(brewery => {
-        try {
-          const marker = new google.maps.Marker({
-            position: { lat: parseFloat(brewery.latitude), lng: parseFloat(brewery.longitude) },
-            map: map,
-            title: brewery.name,
-          });
-  
-          const infoWindow = new google.maps.InfoWindow({
-            content: `<p><strong>${brewery.name}</strong><br>${brewery.street}<br>${brewery.city}, ${brewery.state} ${brewery.postal_code}</p>`,
-          });
-  
-          marker.addListener("click", () => {
-            infoWindow.open(map, marker);
-          });
-        } catch (error) {
-          console.error(error);
-          alert("No breweries found in this location.");
-        }
-      });
-    }
-  
-    function focusMap(breweries) {
-      if (breweries.length === 1) {
-        const brewery = breweries[0];
-        const position = { lat: parseFloat(brewery.latitude), lng: parseFloat(brewery.longitude) };
-        map.setCenter(position);
-        map.setZoom(15);
-      } else {
-        const bounds = new google.maps.LatLngBounds();
-        breweries.forEach(brewery => {
-          const position = { lat: parseFloat(brewery.latitude), lng: parseFloat(brewery.longitude) };
-          bounds.extend(position);
-        });
-        map.fitBounds(bounds);
-      }
-    }
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+}
 
-    var name = document.querySelector("#first-name");
-    var email = document.querySelector("#email");
-    var saveButton = document.getElementById("save");
+const brewLocation = 'https://api.openbrewerydb.org/v1/breweries';
 
-    saveButton.addEventListener("click", function(event) {
-      event.preventDefault();
-
-    var subscriber = {
-      subscriberName: name.value.trim(),
-      subscriberEmail: email.value.trim(),
-    }
-    
-    localStorage.setItem("subscriber", JSON.stringify(subscriber));
-    console.log (subscriber);
-
-
-    var allInputs = document.querySelectorAll('input');
-allInputs.forEach(singleInput => singleInput.value = '');
-
- 
-Swal.fire('Welcome to the Crew!')
+fetch(brewLocation)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    initMap(data);
+  })
+  .catch(error => {
+    console.error(error);
+    alert("Failed to fetch brewery data.");
   });
 
+// Initialize and add the map
+let map;
+
+async function initMap(breweries) {
+  const position = { lat: 40.776, lng: -73.971 };
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), {
+    zoom: 13,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  const searchButton = document.getElementById("search-button");
+  searchButton.addEventListener("click", handleSearch);
+
+  function handleSearch(event) {
+    event.preventDefault();
+    const searchInput = document.getElementById("search-input");
+    const searchQuery = searchInput.value.trim().toLowerCase();
+
+    if (searchQuery === "") {
+      alert("Please enter a state.");
+      return;
+    }
+
+    const filteredBreweries = breweries.filter(brewery => {
+      const state = brewery.state.toLowerCase();
+      return state.includes(searchQuery);
+    });
+
+    if (filteredBreweries.length === 0) {
+      alert("No breweries found in this location.");
+      return;
+    }
+
+    showBreweries(filteredBreweries);
+    focusMap(filteredBreweries);
+  }
+
+  function showBreweries(breweries) {
+    breweries.forEach(brewery => {
+      try {
+        const marker = new google.maps.Marker({
+          position: { lat: parseFloat(brewery.latitude), lng: parseFloat(brewery.longitude) },
+          map: map,
+          title: brewery.name,
+        });
+
+        const infoWindow = new google.maps.InfoWindow({
+          content: `<p><strong>${brewery.name}</strong><br>${brewery.street}<br>${brewery.city}, ${brewery.state} ${brewery.postal_code}</p>`,
+        });
+
+        marker.addListener("click", () => {
+          infoWindow.open(map, marker);
+        });
+      } catch (error) {
+        console.error(error);
+        alert("No breweries found in this location.");
+      }
+    });
+  }
+
+  function focusMap(breweries) {
+    if (breweries.length === 1) {
+      const brewery = breweries[0];
+      const position = { lat: parseFloat(brewery.latitude), lng: parseFloat(brewery.longitude) };
+      map.setCenter(position);
+      map.setZoom(15);
+    } else {
+      const bounds = new google.maps.LatLngBounds();
+      breweries.forEach(brewery => {
+        const position = { lat: parseFloat(brewery.latitude), lng: parseFloat(brewery.longitude) };
+        bounds.extend(position);
+      });
+      map.fitBounds(bounds);
+    }
+  }
+
+  var name = document.querySelector("#first-name");
+  var email = document.querySelector("#email");
+  var saveButton = document.getElementById("save");
+
+  saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+  var subscriber = {
+    subscriberName: name.value.trim(),
+    subscriberEmail: email.value.trim(),
+  }
+  
+  localStorage.setItem("subscriber", JSON.stringify(subscriber));
+  console.log (subscriber);
+
+
+  var allInputs = document.querySelectorAll('input');
+allInputs.forEach(singleInput => singleInput.value = '');
+
+
+Swal.fire('Welcome to the Crew!')
+});
+
 }
-  
-  
-
-
-  
-  
-  
-
 
 
 
